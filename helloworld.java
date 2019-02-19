1357cf967e249b98f3b46ad22af39c9669878431
Gangishetti <42598187+Gangishetti@users.noreply.github.com>
2019-02-20 01:06:12 +0530

    ok

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    //prints "hello, World" in the terminal window.
-    system.out println("Hello, World"):
+    //prints "Hello, World" in the terminal window.
+    system.out println("Hello, World");
   }
 }
